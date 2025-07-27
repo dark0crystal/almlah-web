a3ed773b8ff9f4b010c00e2940fce7472380a197
ALMardasDEV <albusaidi9094@gmail.com>
2025-07-27 15:24:12 +0400

    brand component

--- frontend/src/components/navbar/Brand.tsx
+++ frontend/src/components/navbar/Brand.tsx
@@ -1,7 +1,20 @@
-export default function Brand() {
+import { getTranslations } from "next-intl/server";
+import { Link } from "@/i18n/navigation";
+import { Lalezar } from "next/font/google";
+import { getLocale } from "next-intl/server";
+
+const lalezarFont = Lalezar({
+    weight: "400",
+    subsets: ["latin"],
+    display: "swap",
+  });
+
+export default async function Brand(){
+    const locale = (await getLocale()).substring(0,2)
+    const t = await getTranslations("Links")
     return(
-        <div>
-           <h1></h1>
+        <div className="mx-6 text-4xl ">
+            <Link locale={locale} className={lalezarFont.className} href='/'>{t("brand")}</Link>
         </div>
     )
 }
