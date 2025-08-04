c979a90154b387b16c41a36aa4b5ae4477b3adc0
ALMardasDEV <albusaidi9094@gmail.com>
2025-08-05 01:21:54 +0400

    property model

---
+++ backend/internals/models/property.go
@@ -0,0 +1,27 @@
+// models/property.go
+package models
+
+import "time"
+
+type Property struct {
+	ID           uint      `json:"id" gorm:"primaryKey"`
+	Name         string    `json:"name" gorm:"unique;not null"`
+	Description  string    `json:"description"`
+	Icon         string    `json:"icon"`
+	PropertyType string    `json:"property_type"`
+	CreatedAt    time.Time `json:"created_at"`
+
+	// Relationships
+	PlaceProperties []PlaceProperty `json:"place_properties"`
+}
+
+type PlaceProperty struct {
+	PlaceID    uint      `json:"place_id" gorm:"primaryKey"`
+	PropertyID uint      `json:"property_id" gorm:"primaryKey"`
+	Value      string    `json:"value"`
+	AddedAt    time.Time `json:"added_at" gorm:"default:CURRENT_TIMESTAMP"`
+
+	// Relationships
+	Place    Place    `json:"place" gorm:"foreignKey:PlaceID"`
+	Property Property `json:"property" gorm:"foreignKey:PropertyID"`
+}
