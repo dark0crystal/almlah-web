85db5a8c1fe3981a2837cf3b934ef98c4d056f23
ALMardasDEV <albusaidi9094@gmail.com>
2025-08-05 10:46:45 +0400

    auth dto

---
+++ backend/internals/dto/auth_dto.go
@@ -0,0 +1,28 @@
+package dto
+
+type RegisterRequest struct {
+	Username  string `json:"username" validate:"required,min=3,max=50"`
+	Email     string `json:"email" validate:"required,email"`
+	Password  string `json:"password" validate:"required,min=6"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type AuthResponse struct {
+	Token string   `json:"token"`
+	User  UserInfo `json:"user"`
+}
+
+type UserInfo struct {
+	ID        uint   `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	UserType  string `json:"user_type"`
+}
