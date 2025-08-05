cdb33eaadf3d6b6c5b08f111e68a7a380be6380f
ALMardasDEV <albusaidi9094@gmail.com>
2025-08-05 23:39:20 +0400

    auth service

---
+++ backend/internals/services/auth_service.go
@@ -0,0 +1,89 @@
+package services
+
+import (
+	"almlah/config"
+	"almlah/internals/dto"
+	"almlah/internals/domain"
+	"almlah/internals/utils"
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Register(req dto.RegisterRequest) (*dto.AuthResponse, error) {
+	// Check if user exists
+	var existingUser domain.User
+	if err := config.DB.Where("email = ? OR username = ?", req.Email, req.Username).First(&existingUser).Error; err == nil {
+		return nil, errors.New("user already exists")
+	}
+
+	// Hash password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create user
+	user := domain.User{
+		Username:     req.Username,
+		Email:        req.Email,
+		PasswordHash: string(hashedPassword),
+		FirstName:    req.FirstName,
+		LastName:     req.LastName,
+		UserType:     "regular",
+		IsActive:     true,
+	}
+
+	if err := config.DB.Create(&user).Error; err != nil {
+		return nil, err
+	}
+
+	// Generate JWT
+	token, err := utils.GenerateJWT(user.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &dto.AuthResponse{
+		Token: token,
+		User: dto.UserInfo{
+			ID:        user.ID,
+			Username:  user.Username,
+			Email:     user.Email,
+			FirstName: user.FirstName,
+			LastName:  user.LastName,
+			UserType:  user.UserType,
+		},
+	}, nil
+}
+
+func Login(req dto.LoginRequest) (*dto.AuthResponse, error) {
+	var user domain.User
+	if err := config.DB.Where("email = ?", req.Email).First(&user).Error; err != nil {
+		return nil, errors.New("invalid credentials")
+	}
+
+	// Check password
+	if err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(req.Password)); err != nil {
+		return nil, errors.New("invalid credentials")
+	}
+
+	// Generate JWT
+	token, err := utils.GenerateJWT(user.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &dto.AuthResponse{
+		Token: token,
+		User: dto.UserInfo{
+			ID:        user.ID,
+			Username:  user.Username,
+			Email:     user.Email,
+			FirstName: user.FirstName,
+			LastName:  user.LastName,
+			UserType:  user.UserType,
+		},
+	}, nil
+}
+
