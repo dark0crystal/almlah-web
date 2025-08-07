2e5fb4ddff9f8d99fb392a04db95c47fd8e94d98
ALMardasDEV <albusaidi9094@gmail.com>
2025-08-07 23:36:27 +0400

    update the auth dto

--- backend/internals/dto/auth_dto.go
+++ backend/internals/dto/auth_dto.go
@@ -1,13 +1,17 @@
 package dto
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"time"
+)
 
+// Authentication requests
 type RegisterRequest struct {
 	Username  string `json:"username" validate:"required,min=3,max=50"`
 	Email     string `json:"email" validate:"required,email"`
 	Password  string `json:"password" validate:"required,min=6"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+	FirstName string `json:"first_name" validate:"max=100"`
+	LastName  string `json:"last_name" validate:"max=100"`
 }
 
 type LoginRequest struct {
@@ -15,16 +19,71 @@ type LoginRequest struct {
 	Password string `json:"password" validate:"required"`
 }
 
+type GoogleAuthRequest struct {
+	Token string `json:"token" validate:"required"`
+}
+
+type ForgotPasswordRequest struct {
+	Email string `json:"email" validate:"required,email"`
+}
+
+type ResetPasswordRequest struct {
+	Token    string `json:"token" validate:"required"`
+	Password string `json:"password" validate:"required,min=6"`
+}
+
+type VerifyEmailRequest struct {
+	Token string `json:"token" validate:"required"`
+}
+
+type ChangePasswordRequest struct {
+	CurrentPassword string `json:"current_password" validate:"required"`
+	NewPassword     string `json:"new_password" validate:"required,min=6"`
+}
+
+type UpdateProfileRequest struct {
+	Username  string `json:"username" validate:"omitempty,min=3,max=50"`
+	FirstName string `json:"first_name" validate:"max=100"`
+	LastName  string `json:"last_name" validate:"max=100"`
+}
+
+// Authentication responses
 type AuthResponse struct {
-	Token string   `json:"token"`
-	User  UserInfo `json:"user"`
+	Token     string    `json:"token"`
+	ExpiresAt time.Time `json:"expires_at"`
+	User      UserInfo  `json:"user"`
 }
 
 type UserInfo struct {
-	ID        uuid.UUID `json:"id"`
-	Username  string    `json:"username"`
-	Email     string    `json:"email"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	UserType  string    `json:"user_type"`
+	ID           uuid.UUID `json:"id"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	FirstName    string    `json:"first_name"`
+	LastName     string    `json:"last_name"`
+	FullName     string    `json:"full_name"`
+	ProfilePic   string    `json:"profile_picture"`
+	UserType     string    `json:"user_type"`
+	Provider     string    `json:"provider"`
+	IsVerified   bool      `json:"is_verified"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+type GoogleUserInfo struct {
+	ID            string `json:"id"`
+	Email         string `json:"email"`
+	VerifiedEmail bool   `json:"verified_email"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Picture       string `json:"picture"`
+	Locale        string `json:"locale"`
+}
+
+// Generic responses
+type MessageResponse struct {
+	Message string `json:"message"`
+}
+
+type ProfileResponse struct {
+	User UserInfo `json:"user"`
 }
