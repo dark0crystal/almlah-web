f3464fd2aca8f480e99013ee85df1f0a54b1882a
ALMardasDEV <albusaidi9094@gmail.com>
2025-08-07 21:09:09 +0400

    make changes to the favorities model

--- backend/internals/domain/favorites.go
+++ backend/internals/domain/favorites.go
@@ -1,15 +1,27 @@
-// models/favorites.go
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type UserFavorite struct {
-	ID      uint      `json:"id" gorm:"primaryKey"`
-	UserID  uint      `json:"user_id" gorm:"not null"`
-	PlaceID uint      `json:"place_id" gorm:"not null"`
+	ID      uuid.UUID `json:"id" gorm:"type:uuid;primaryKey"`
+	UserID  uuid.UUID `json:"user_id" gorm:"type:uuid;not null"`
+	PlaceID uuid.UUID `json:"place_id" gorm:"type:uuid;not null"`
 	AddedAt time.Time `json:"added_at" gorm:"default:CURRENT_TIMESTAMP"`
 
 	// Relationships
-	User  User  `json:"user" gorm:"foreignKey:UserID"`
-	Place Place `json:"place" gorm:"foreignKey:PlaceID"`
+	User  User  `json:"user" gorm:"foreignKey:UserID;references:ID"`
+	Place Place `json:"place" gorm:"foreignKey:PlaceID;references:ID"`
+}
+
+// BeforeCreate hook to generate UUID
+func (uf *UserFavorite) BeforeCreate(tx *gorm.DB) error {
+	if uf.ID == uuid.Nil {
+		uf.ID = uuid.New()
+	}
+	return nil
 }
