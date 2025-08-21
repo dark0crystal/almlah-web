3123300ff57838a99f9bd9c25213c5218892f7e2
ALMardasDEV <albusaidi9094@gmail.com>
2025-08-21 23:55:41 +0400

    data and galleries

---
+++ frontend/src/app/[locale]/gallery/Galleries.tsx
@@ -0,0 +1,35 @@
+'use client';
+import { useState ,useEffect} from "react";
+import Scene from './Scene';
+import Projects from './Projects';
+import Lenis from 'lenis'
+
+
+const Galleries: React.FC = () => {
+
+  useEffect( ()=>{
+    const lenis = new Lenis()
+    function raf(time: number) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+  },[])
+  
+
+  const [activeMenu, setActiveMenu] = useState<number | null>(null);
+
+  return (
+    <div className="relative">
+      <div className="relative z-10 mix-blend-difference">
+        <Projects setActiveMenu={setActiveMenu} />
+      </div>
+      <div className="fixed top-0 left-0 w-full h-screen -z-10 ">
+        <Scene activeMenu={activeMenu} />
+      </div>
+      <div className="h-[100vh]"></div>
+    </div>
+  );
+}
+
+export default Galleries;
---
+++ frontend/src/app/[locale]/gallery/data.ts
@@ -0,0 +1,42 @@
+export const projects = [
+    {
+      title: " العروبة العريقة",
+      src: "/gallery/img14.jpg"
+    },
+    {
+      title: "بوابة السماء",
+      src: "/gallery/img16.jpg"
+    },
+    {
+      title: "بين وبين  / in between",
+      src: "/gallery/img15.jpg"
+    },
+    {
+      title: "سكون مزعج  /  Disturbing Serenity",
+      src: "/gallery/img17.jpg"
+    },
+    {
+      title: "رالي داكار السعودي",
+      src: "/img5.jpeg"
+    },
+    {
+      title: "معزوفة الليل  / Symphony of the night ",
+      src: "/gallery/img3.jpg"
+    },
+    {
+      title: "بحيرات الصفا",
+      src: "/gallery/img18.jpg"
+    },
+    {
+      title: "المرموقة / SQU",
+      src: "/gallery/img12.jpg"
+    },
+    {
+      title: "أرشيفية من البوسنة",
+      src: "/img2.jpeg"
+    },
+    {
+      title: "عظمة",
+      src: "/gallery/img20.jpg"
+    }
+]
