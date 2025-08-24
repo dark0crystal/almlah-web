3c7990708a33c45ca90d2aff9495a21c4ef3d252
ALMardasDEV <albusaidi9094@gmail.com>
2025-08-24 15:33:39 +0400

    updating images import

---
+++ frontend/src/app/[locale]/the-gallery/data.ts
@@ -0,0 +1,42 @@
+export const projects = [
+    {
+      title: " العروبة العريقة",
+      src: "/gallery-alkhalil/img14.jpg"
+    },
+    {
+      title: "بوابة السماء",
+      src: "/gallery-alkhalil/img16.jpg"
+    },
+    {
+      title: "بين وبين  / in between",
+      src: "/gallery-alkhalil/img15.jpg"
+    },
+    {
+      title: "سكون مزعج  /  Disturbing Serenity",
+      src: "/gallery-alkhalil/img17.jpg"
+    },
+    {
+      title: "رالي داكار السعودي",
+      src: "/gallery-alkhalil/img3.jpg"
+    },
+    {
+      title: "معزوفة الليل  / Symphony of the night ",
+      src: "/gallery-alkhalil/img3.jpg"
+    },
+    {
+      title: "بحيرات الصفا",
+      src: "/gallery-alkhalil/img18.jpg"
+    },
+    {
+      title: "المرموقة / SQU",
+      src: "/gallery-alkhalil/img12.jpg"
+    },
+    {
+      title: "أرشيفية من البوسنة",
+      src: "/gallery-alkhalil/img3.jpg"
+    },
+    {
+      title: "عظمة",
+      src: "/gallery-alkhalil/img20.jpg"
+    }
+]
