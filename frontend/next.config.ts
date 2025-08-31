38f2b43ded09884c43be69b719f6ec1946a83355
ALMardasDEV <albusaidi9094@gmail.com>
2025-08-31 11:00:02 +0400

    removing docker from the frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,17 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    images: {
+    domains: [
+      'jeyypngrycucoystlmft.supabase.co',
+      'lh3.googleusercontent.com',
+      'images.unsplash.com',
+      'localhost',
+      '127.0.0.1'
+    ],
+  },
+};
  
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
