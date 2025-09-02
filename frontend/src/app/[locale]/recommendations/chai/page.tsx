5cde80bbb4e16f6c89dffe43f17b736a2487973e
ALMardasDEV <albusaidi9094@gmail.com>
2025-09-02 16:24:04 +0400

    create a breakfast recomendation page

---
+++ frontend/src/app/[locale]/recommendations/chai/page.tsx
@@ -0,0 +1,9 @@
+import RecommendationImage from "../components/RecommendationImage";
+
+export default function Chai(){
+    return(
+        <div className="max-w-6xl mx-auto px-4 py-12">   
+            <RecommendationImage src="/chai-hero.jpg" alt="chai"/>
+        </div>
+    )
+}
